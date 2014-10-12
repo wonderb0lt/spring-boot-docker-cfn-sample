ff644920a0589adb86164de04e7a68f8a51f02e2
Patrick Stegmann <code@patrick-stegmann.de>
2014-10-12 18:26:35 +0200

    No debug in docker.sh, warning if no VERSION was passed

--- docker.sh
+++ docker.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 set -e
-set -x
 
 # Main configuration for docker run. In addition to this, you need to pass "VERSION" as an environment variable.
+
+if [ -z $VERSION ]; then
+    echo "*** You need to pass VERSION as an environment variable ***"
+    exit 1
+fi
+
 REGISTRY="some-docker-registry.elasticbeanstalk.com"
 APP_NAME="spring-boot-docker-cfn-sample"
 DESCRIPTION="Spring Boot + Docker + CloudFormation sample"
